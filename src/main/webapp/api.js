3018cc3297b4ecda27758762a7122c65aeb717b2
vixxjy <ajorvictor48@gmail.com>
2022-10-02 22:48:06 +0100

    [pharmacy] patientcard updates

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,5 +1,5 @@
 
 //export const  token = (new URLSearchParams(window.location.search)).get("jwt")
 //export const url = '/api/'
-export const url =  'http://localhost:8281/api/';
-export const token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjYwMjYwOTQ3fQ.2GNuw3hJYL4Mw1YipUomKPKGJ3Naj0YXKB_Ltet3Ri7lM3_J2ZOec-kdffiQAlhau26EsKwtZf9vPbBSqRqckg';
+export const url =  'http://localhost:8282/api/v1/';
+export const token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjY0NTczMTQzfQ.xMM0h0iEGaLdenrXKkNMcE2chwUhCdOAk0oawtau9ybDCRc_X2P-kTr2I1GLeCwy1b6aOrrJx2mpZxXmcKstKw';
